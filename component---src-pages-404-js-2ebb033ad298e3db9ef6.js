a62946958a5e164a4eb0d12029d41ed97cbc3d35
elevyg <elevyg91@gmail.com>
2021-09-08 10:48:15 -0300

    Updates

---
+++ component---src-pages-404-js-2ebb033ad298e3db9ef6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),o=t("Bl7J"),u=t("vrFN");n.default=function(){return l.a.createElement(o.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"No encontramos tu página"),l.a.createElement("p",null,"Lamentablemente la página que estas buscando no existe"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2ebb033ad298e3db9ef6.js.map
